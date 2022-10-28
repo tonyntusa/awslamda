5e637755c4e3799222264f93d360da9ef0f8a85e
tonyntusa <114531666+tonyntusa@users.noreply.github.com>
2022-10-27 23:11:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
-#-*- coding: utf-8 -*-
-__author__ = "Chirag Rathod (Srce Cde)"
-__license__ = "MIT"
-__email__ = "chiragr83@gmail.com"
-__maintainer__ = "Chirag Rathod (Srce Cde)"
-
 import json
 import requests
 
 def handler(event, context):
-
-    # TODO implementation
-    
     return {
-        'headers': {'Content-Type' : 'application/json'},
-        'statusCode': 200,
-        'body': json.dumps({"message": "Lambda Container image invoked!",
-                            "event": event})
-    }
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
